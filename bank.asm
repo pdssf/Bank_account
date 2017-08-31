367686443ae7346413d4852c6b42c73af490e21c
Paulo Diogenes Silva Salgado Filho <pdssf@uhwc04.windows.cin.ufpe.br>
2017-08-31 17:17:49 -0300

    adicionando parte da funcao de listar agencias

--- bank.asm
+++ bank.asm
@@ -9,24 +9,41 @@ STRUC cliente
 .validade resb 1
 ENDSTRUC
 
+;campo de declaracao de string
+
+NotAccount db "conta nao encontrada",10,13,0
+concluido db "feito!"10,13,0
+
+
 aCliente: times 10*cliente_size db 0
 aSize: dw 10
 sSize: dw 28
+ag_num times aSize dw 0 					;guarda o num de uma agencia
+bitVal db 0;								;ajuda a tomar validade
 
 start:
-	lea si, [aCliente]
+	xor ax,ax
+	mov ds,ax
 
+; ps. construir menu de escolha: 
+
+
+;========================================criando uma conta:
+
+	lea si, [aCliente]
 busca:
-	push si ;
+	push si 
 	push ax
 	push bp
-	push cx ;salva o valor original dos registradores a serem usados pelo call
-	mov bp, sp ;copia sp em bp para ficar mais facil de trabalhar
-	add bp, 10 ;adicionando 10 a dp é possivel ter acesso ao endereço dos parametros a serem usados pelo call
-	mov ax, [bp] ;carrega o numero a ser buscado (x)
-	mov si, [bp + 2] ;carrega o endereço onde deve ser iniciado a busca (assume-se que v[0])
+	push cx 													;salva o valor original dos registradores a serem usados pelo call
 
-	mov cx, [aSize] ;tamanho do vetor pra ser usado na pilha
+	mov bp, sp 													;copia sp em bp para ficar mais facil de trabalhar
+	add bp, 10 													;adicionando 10 a dp é possivel ter acesso ao endereço dos parametros a serem usados pelo call
+	mov ax, [bp] 												;carrega o numero a ser buscado (x)
+	mov si, [bp + 2] 											;carrega o endereço onde deve ser iniciado a busca (assume-se que v[0])
+
+	mov cx, [aSize] 											;tamanho do vetor pra ser usado na pilha
+
 	compara:
 		cmp ax, [si + cliente.conta] ;x == v[i]?
 		je salvaEndereco ;se sim, sai do loop
@@ -35,8 +52,8 @@ busca:
 		loop compara
 
 	mov si, string 
-	call print_string ;caso a conta buscada não exista, informa ao usuario e
-	mov [bp + 2], -1 ;salva -1 na posição da pilha onde originalmente estava v[0]
+	call print_string 											;caso a conta buscada não exista, informa ao usuario e
+	mov [bp + 2], -1 											;salva -1 na posição da pilha onde originalmente estava v[0]
 	jmp fimBusca
 
 	salvaEndereco:
@@ -48,6 +65,8 @@ busca:
 		pop ax
 		pop si ;restaura os valores originais dos registradores usados no call
 		ret 2 ;retorna incrementando sp em 2 para sobrescrever o local onde estava o parametro x
+
+;========================================deletando uma conta:
 
 deleta:
 	push si ;
@@ -74,6 +93,27 @@ deleta:
 		pop si ;restaura os valores originais do registrador
 		ret 4 ;retorna sobrescrevendo os paramentros contidos na pilha
 
+;========================================listar agencias:
+
+	lea si, [aCliente+ cliente.agencia]						;move primeira conta para si: deslocando o tamanho ate agencia
+	mov bx, si
+	add bx, 2												;faz bx apontar para a validade
+	mov byte[bitVal],bx
+	mov cx, aSize											;move para cx o numero de vetores
+
+ag_busca:
+	cmp byte[bitVal],0
+	je notprint											
+
+	mov ax, [si]										;move para ax o conteudo apontado por si
+	mov world[ag_num], ax
+	call print_number									;chama o procedimendo para imprimir numero
+					;assumindo que o primeiro numero ja foi impresso, prosseguimos para o proximo
+	
+	notprint:											;caso nao precise printar
+
+	add si, world[sSize]								;avança para a proxima
+loop ag_busca
 
 end:
 times 510 - ($ - $$) db 0
