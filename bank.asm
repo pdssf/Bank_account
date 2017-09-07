0f69535ecf0c4d2ad7c8bb42eb3919900053ca13
mgm6 <mgm6@cin.ufpe.br>
2017-09-07 14:38:44 -0300

    bank.asm agora compila e exibe menu

--- bank.asm
+++ bank.asm
@@ -1,5 +1,8 @@
-org 0x7E00
+org 0x7C00
 jmp 0x0000:start
+
+main_menu db 'escolha sua opcao', 10, 13,'1-casdastrar nova conta', 10, 13, '2-buscar conta', 10, 13, '3-editar conta', 10, 13, '4-deletar conta', 10, 13, '5-listar agencias', 10, 13, '6-listar contas', 10, 13,0
+option db 'Name:', 10,13,0
 
 STRUC register
     .name resb 21
@@ -21,7 +24,6 @@ client: ISTRUC register
 IEND
 
 ;string declaration field
-option db "Name:", 10,13,0
 menu_str db 'Choose your option', 10, 13,'1 - Register New Account', 10, 13, '2 - Query Account', 10, 13, '3 - Edit Account', 10, 13, '4 - Delete Account', 10, 13, '5 - List Agencies', 10, 13, '6 - List Accounts', 10, 13,0
 array_size: dw 10
 
@@ -32,17 +34,24 @@ client_size:	EQU ($ - client_array) / register.size
 
 SEGMENT .text
 start:
-    xor ax,ax
-    mov ds,ax
-    
-	 mov ah, 0 						   ;enters the video mode
-	 mov al, 12h
-	 int 10h
+   xor ax,ax
+   mov ds,ax
+    	
+	;mode 12h (video)
+   mov ah, 0x0
+   mov al,12h
+   int 10h
+   
+   ;fundo azul
+   mov ah, 0xb
+   mov bh, 0
+   mov bl, 1
+   int 10h
 	
 menu:
-    mov si, menu_str
+    mov si, main_menu
     print_menu:
-    	lodsb                          ;loads a byte from DS:SI into AL and then increments SI
+    	lodsb                          ;loads a byte from DS:SI into AL and then increments SI  
     	mov ah, 0xe                    ;code of the instruction to print a char which is in al
     	mov bl,0xf
     	mov bh,0
@@ -52,7 +61,7 @@ menu:
 		je done_menu
     jmp print_menu
 done_menu:
-
+	
     mov ah, 0
     int 16h
     sub al, '0'
@@ -66,14 +75,14 @@ done_menu:
     cmp al, 4                           ;4 - Delete Account
     
     cmp al, 5                           ;5 - List Agencies
-    	call list_agencies
+    	;call list_agencies
     cmp al, 6                           ;6 - List Accounts
-    	call list_accounts
+    	;call list_accounts
 jmp menu
 
 ;======================================= Registers a new account:
 create:
-	mov di, option 			           ;/*Name: ...*/ 
+	mov si, option 			           ;/*Name: ...*/ 
 	call print_string			       ;prints "Name:"
 	call searches      			       ;searchs for an empty slot in the structure
 	;cmp si, vec_size				   ;compares si with vec_size
@@ -94,112 +103,142 @@ ret
 find_account:
     ;lea si, [client_array]                 ;vai para a primeira posição do vetor de contas
 
-    push si
-    push ax
-    push bp
-    push cx 	;salva o valor original dos registradores a serem usados pelo call
+    ;push si
+    ;push ax
+    ;push bp
+    ;push cx 	;salva o valor original dos registradores a serem usados pelo call
 
-    mov bp, sp 	;copia sp em bp para ficar mais facil de trabalhar
-    add bp, 10 ;adicionando 10 a dp é possivel ter acesso
+    ;mov bp, sp 	;copia sp em bp para ficar mais facil de trabalhar
+    ;add bp, 10 ;adicionando 10 a dp é possivel ter acesso
     				;ao endereço dos parametros a serem usados pelo call
-    mov ax, [bp] 			;carrega o numero a ser buscado (x)
-    mov si, [bp + 2] 	;carrega o endereço onde deve ser iniciado a busca (assume-se que v[0])
+    ;mov ax, [bp] 			;carrega o numero a ser buscado (x)
+    ;mov si, [bp + 2] 	;carrega o endereço onde deve ser iniciado a busca (assume-se que v[0])
 
-    mov cx, [array_size] 											;tamanho do vetor pra ser usado na pilha
+    ;mov cx, [array_size] 											;tamanho do vetor pra ser usado na pilha
 
-    compara:
-        cmp ax, [si + register.account] ;x == v[i]?
-        je salvaEndereco ;se sim, sai do loop
+    ;compara:
+        ;cmp ax, [si + register.account] ;x == v[i]?
+        ;je salvaEndereco ;se sim, sai do loop
 
-        add si, [client_size] ;se não, i++
-    loop compara
+        ;add si, [client_size] ;se não, i++
+        ;loop compara
 
-    mov si, string 
-    call print_string 	;caso a conta buscada não exista, informa ao usuario e
-    mov [bp + 2], -1 	;salva -1 na posição da pilha onde originalmente estava v[0]
-    jmp fimBusca
+    ;mov si, string 
+    ;call print_string 	;caso a conta buscada não exista, informa ao usuario e
+    ;mov [bp + 2], -1 	;salva -1 na posição da pilha onde originalmente estava v[0]
+    ;jmp fimBusca
 
-    salvaEndereco:
-        mov [bp + 2], si ;salva v[i] na posição da pilha onde antes existia v[0]
+    ;salvaEndereco:
+        ;mov [bp + 2], si ;salva v[i] na posição da pilha onde antes existia v[0]
 
-    fimBusca:
-        pop cx ;
-        pop bp
-        pop ax
-        pop si ;restaura os valores originais dos registradores usados no call
-        ret 2 ;retorna incrementando sp em 2 para sobrescrever o local onde estava o parametro x
-ret
+    ;fimBusca:
+        ;pop cx ;
+        ;pop bp
+        ;pop ax
+        ;pop si ;restaura os valores originais dos registradores usados no call
+        ;ret 2 ;retorna incrementando sp em 2 para sobrescrever o local onde estava o parametro x
+;ret
 ;========================================deletando uma conta:
 
-    deleta:
-    push si ;
-    push ax
-    push bp ;salva o valor original dos registradores a serem usados pelo call
-    mov bp, sp ;copia sp em bp para ficar mais facil de trabalhar
-    add bp, 8 	;adicionando 8 a dp é possivel ter acesso ao endereco
+    ;deleta:
+    ;push si ;
+    ;push ax
+    ;push bp ;salva o valor original dos registradores a serem usados pelo call
+    ;mov bp, sp ;copia sp em bp para ficar mais facil de trabalhar
+    ;add bp, 8 	;adicionando 8 a dp é possivel ter acesso ao endereco
     				; dos parametros a serem usados pelo call
-    mov ax, [bp] ;carrega o numero da conta a ser deletado
-    mov si, [bp + 2] ;carrega o endereco da posicao inicial do vetor de clientes
+    ;mov ax, [bp] ;carrega o numero da conta a ser deletado
+    ;mov si, [bp + 2] ;carrega o endereco da posicao inicial do vetor de clientes
 
-    push si ;
-    push ax ;salva o numero da conta e o endereco de v[0] pra ser usado no procedimento busca
-    call busca
+    ;push si ;
+    ;push ax ;salva o numero da conta e o endereco de v[0] pra ser usado no procedimento busca
+    ;call busca
 
-    pop si
-    cmp si, -1 
-    jne fimDeleta ;se busca retornar -1, ou seja, a conta não existe pula pro final
+    ;pop si
+    ;cmp si, -1 
+    ;jne fimDeleta ;se busca retornar -1, ou seja, a conta não existe pula pro final
 
-    mov word[si + register.validity], 0 ;caso contrario sobrescreve o bit validade 
+    ;mov word[si + register.validity], 0 ;caso contrario sobrescreve o bit validade 
     												;"apagando" a conta
 
-    fimDeleta:
-        pop bp ;
-        pop ax
-        pop si ;restaura os valores originais do registrador
-        ret 4 ;retorna sobrescrevendo os paramentros contidos na pilha
+    ;fimDeleta:
+        ;pop bp ;
+        ;pop ax
+        ;pop si ;restaura os valores originais do registrador
+        ;ret 4 ;retorna sobrescrevendo os paramentros contidos na pilha
 
 ;========================================listar agencias:
-    list_agencies:
+    ;list_agencies:
     
-    lea dx, [client_array]	;move primeira conta para si: deslocando o tamanho
+    ;lea si, [client_array+ register.agency]	;move primeira conta para si: deslocando o tamanho
     													; ate agencia
-    mov cx, word[array_size]				;move para cx o numero de elementos no vetor
+    ;mov cx, array_size				;move para cx o numero de elementos no vetor
 
-    ag_busca:
-        lea bx, [dx+register.validity]     ;carrega o bit de validade em bx
-        cmp word[bx],0		;verifica se esta livre
-        je notprint        ;caso não seja uma posicao valida
-        lea si,[dx+ register.agency] ;SI now point to the agency number
-    back:
-        call print_string	;chama o procedimendo para imprimir numero
-    notprint:					;caso nao precise printar
-        add dx, word[client_size];avança para a proxima(si+28)
-        loop ag_busca
-    ret
-  
+    ;ag_busca:
+        ;lea bx, [si+4]     ;carrega o bit de validade em bx
+        ;cmp word[bx],0		;verifica se esta livre
+        ;je notprint        ;caso não seja uma posicao valida
+
+        ;mov ax,[si]        ;movo o numero da agencia para ax
+        ;mov bx, ag_num		;
+        ;call agfetch
+    ;back:
+        ;call print_number	;chama o procedimendo para imprimir numero
+    ;notprint:					;caso nao precise printar
+
+        ;add si, word[client_size];avança para a proxima(si+28)
+        ;loop ag_busca
+        ;jmp menu
+    ;agfetch:            ;compara ax com as contas existentes em ag_num
+        ;cmp ax, word[bx]
+        ;je notprint
+        
+        ;add bx,4
+        ;cmp word[bx],0
+        ;jne agfetch
+        
+        ;mov [bx],ax
+    ;jmp back
+    
 ;========================================listar contas:
-list_accounts:
-    lea dx, [client_array]	;move primeira conta para dx: 
+;list_accounts:
+    ;lea si, [client_array+register.account]	;move primeira conta para si: 
     												;deslocando o tamanho ate conta
-    mov cx, word[array_size]								;move para cx o numero de elementos no vetor
+    ;mov cx, array_size								;move para cx o numero de elementos no vetor
 
-    accountshow:
-        lea bx, [dx+register.validity]
-        cmp word[bx],0
-        je not_acc         ;caso não seja uma posicao valida
-        lea si,[dx+register.account]
-        call print_number   ;chamo o procedimendo para imprimir
-    not_acc:
-        add dx, word[client_size]	;avança para a proxima(si+28)
-    loop accountshow
-ret    
+    ;accountshow:
+        ;lea bx, [si+2]
+        ;cmp word[bx],0
+        ;je semconta         ;caso não seja uma posicao valida
+        ;mov ax,[si]         ;movo o numero da conta para ax
+        ;call print_number   ;chamo o procedimendo para imprimir
+    ;semconta:
+        ;add si, word[client_size]								           ;avança para a proxima(si+28)
+    ;loop accountshow
+;ret
+;========================================listar contas:
+;list_accounts:
+    ;lea dx, [client_array]	;move primeira conta para dx: 
+    												;deslocando o tamanho ate conta
+    ;mov cx, word[array_size]								;move para cx o numero de elementos no vetor
+
+    ;accountshow:
+        ;lea bx, [dx+register.validity]
+        ;cmp word[bx],0
+        ;je not_acc         ;caso não seja uma posicao valida
+        ;lea si,[dx+register.account]
+        ;call print_number   ;chamo o procedimendo para imprimir
+    ;not_acc:
+        ;add dx, word[client_size]	;avança para a proxima(si+28)
+    ;loop accountshow
+;ret    
 ;========================================:
 read_string:	
 	mov ah, 0 	;
 	int 16h 		;  /*AL <- caracter*/				
 	stosb 		;	/* tirar de AL->DI*/	
 	cmp al, 13	;
-	je .read:
+	je .read
 
 	mov ah, 0xe ; /*exibe o que esta sendo escrito na leitura*/
 	mov bl, 2
@@ -235,13 +274,19 @@ searches: ; /*essa funcao procura uma posicao vazia para fazer operacoes (ex:reg
    												; em bx para comparar
    	cmp word[bx], 1						;caso a posicao esteja ocupada, avanca para prox posicao
    	je .invalida        					;caso não seja uma posicao valida
-   	mov [si+register.validity], 1     ;movo o numero da conta para ax
+   	mov byte[si+register.validity], 1     ;movo o numero da conta para ax
    	ret  										;retorna apos preencher a posicao valida
    .invalida:
    	add si, word[client_size]					;avança para a proxima(si+28)
    loop .search_account						;se sair desse loop, nao encontrou espaco   
 ret
 ;========================================:
+print_char:
+	mov ah, 0xe                    ;code of the instruction to print a char which is in al
+   mov bl,0xf
+   mov bh,0
+	int 10h        
+ret
 end:
 times 510 - ($ - $$) db 0
 dw 0xAA55
